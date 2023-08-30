74bd3ea54dd00746ecde1fd8560381c81822d8b4
Franck CICCOTTI <franckciccotti@me.com>
2023-08-30 17:48:19 +0200

    work on  services

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-list-pokemon',
@@ -9,15 +9,21 @@ import { Pokemon } from '../pokemon';
   styles: [
   ]
 })
-export class ListPokemonComponent {
+export class ListPokemonComponent implements OnInit {
 
-  pokemonList: Pokemon[] = POKEMONS;
+  pokemonList: Pokemon[];
 	
-	constructor(private router: Router) {}
+	constructor(
+    private router: Router, 
+    private pokemonService: PokemonService
+  ) { }
+
+  ngOnInit(): void {
+    this.pokemonList = this.pokemonService.getPokemonList();
+  }
 	
 	goToPokemon(pokemon: Pokemon){
 		this.router.navigate(['/pokemon', pokemon.id]);
 	}
-
 
 }
