ebb7a4d2e4a8c2dacfdfac4c33cb2ca0b2ef5d92
Franck CICCOTTI <franckciccotti@me.com>
2023-09-12 21:53:56 +0200

    authentification form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { InMemoryDataService } from './in-memory-data.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+  ]
+})
+export class LoginComponent implements OnInit {
+
+  message: string = 'Vous êtes déconnecté.';
+  name: string;
+  password: string;
+  auth: AuthService;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {}
+
+  ngOnInit(): void {
+    this.auth = this.authService;
+  }
+
+  setMessage() {
+    if (this.auth.isLoggedIn) {
+      this.message = 'Vous êtes connecté.';
+    } else {
+      this.message = 'Identifiant ou mot de passe incorrect.' 
+    }
+  }
+
+  login() {
+    this.message = 'Tentative de connexion en cours... ';
+    this.auth.login(this.name, this.password)
+      .subscribe((isLoggedIn: boolean) => {
+        this.setMessage();
+        if(isLoggedIn) {
+          this.router.navigate(['/pokemons/']);
+        } else {
+          this.password = '';
+          this.router.navigate(['/login']);
+        }
+      })
+  }
+
+  logout() {
+    this.auth.logout();
+    this.message = 'Vous êtes déconnecté.';
+  }
+
+}
